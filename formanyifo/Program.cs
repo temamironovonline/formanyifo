7a10673d34178e095103e248d2dae4600a3d1ccf
temamironovonline <a.sergeichev03@mail.ru>
2022-10-04 19:58:14 +0300

    complete

--- formanyifo/Program.cs
+++ formanyifo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyApp;
+using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -11,7 +12,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         public string signEast;
         public void show()
         {
-            Console.WriteLine("{0,-15} {1,-15} {2,-15} {3, -15} {4, -15}", day, month, year, signZodiac, signZodiac);
+            Console.WriteLine("{0,-15} {1,-15} {2,-15} {3, -15} {4, -15}", day, month, year, signZodiac, signEast);
         }
         public string concat()
         {
@@ -59,15 +60,21 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         static void checkSigns(List<Users> L)
         {
-
             for (int i = 0; i < L.Count; i++)
             {
                 Users u = L[i];
+                int forParse = 0;
 
-                if (u.day == "" || u.month == "" || Convert.ToInt32(u.day) <= 0 || Convert.ToInt32(u.day) > 31 || (Convert.ToInt32(u.day) > 28 && Convert.ToInt32(u.month) == 2) || Convert.ToInt32(u.month) <= 0 || Convert.ToInt32(u.month) > 12)
+                if (u.day == "" || u.month == "" || !int.TryParse(u.day, out forParse) || !int.TryParse(u.month, out forParse))
                 {
-                    u.signZodiac = "Невозможно определить!";
+                    u.signZodiac = "Невозможно определить знак зодиака!";
                 }
+
+                else if (Convert.ToInt32(u.day) <= 0 || Convert.ToInt32(u.day) > 31 || (Convert.ToInt32(u.day) > 28 && Convert.ToInt32(u.month) == 2) || Convert.ToInt32(u.month) <= 0 || Convert.ToInt32(u.month) > 12)
+                {
+                    u.signZodiac = "Невозможно определить знак зодиака!";
+                }
+
                 else
                 {
                     if ((Convert.ToInt32(u.month) == 3 && Convert.ToInt32(u.day) >= 21) || (Convert.ToInt32(u.month) == 4 && Convert.ToInt32(u.day) <= 20))
@@ -120,148 +127,100 @@ namespace MyApp // Note: actual namespace depends on the project name.
                         u.signZodiac = "Рыбы";
                     }
                 }
-                if (u.year == "" || Convert.ToInt32(u.year) < 0 || Convert.ToInt32(u.year) > 9999)
+                if (u.year == "" || !int.TryParse(u.year, out forParse))
                 {
-                    u.signEast = "Невозможно определить!";
+                    u.signEast = "Невозможно определить знак по восточному календарю!";
                 }
                 else
                 {
-                    int year = Convert.ToInt32(birthYear.Text);
                     string[] signs = { "Крыса", "Бык", "Тигр", "Кот", "Дракон", "Змея", "Лошадь", "Коза", "Обезьяна", "Петух", "Собака", "Кабан" };
+
                     int a;
                     int sign = 0;
-                    if (year >= 1900)
+                    if (Convert.ToInt32(u.year) >= 1900)
                     {
                         a = 1899;
-                        while (a < year)
+                        while (a < Convert.ToInt32(u.year))
                         {
-                            for (int i = 0; i < 12 && a < year; i++)
+                            for (int j = 0; j < 12 && a < Convert.ToInt32(u.year); j++)
                             {
                                 a++;
-                                sign = i;
+                                sign = j;
                             }
                         }
                     }
-                    if (year < 1900)
+                    else if (Convert.ToInt32(u.year) < 1900)
                     {
                         a = 1900;
-                        while (a > year)
+                        while (a > Convert.ToInt32(u.year))
                         {
-                            for (int i = 11; i > 0 && a > year; i--)
+                            for (int j = 11; j > 0 && a > Convert.ToInt32(u.year); j--)
                             {
                                 a--;
-                                sign = i;
+                                sign = j;
                             }
                         }
                     }
-
-                    if (sign == 0)
+                    switch(sign)
                     {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/rat.png"));
-                        textVost.Text = "КРЫСА";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 1)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/bull.png"));
-                        textVost.Text = "БЫК";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 2)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/tiger.png"));
-                        textVost.Text = "ТИГР";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 3)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/rabbit.png"));
-                        textVost.Text = "КРОЛИК";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 4)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/dragon.png"));
-                        textVost.Text = "ДРАКОН";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 5)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/snake.png"));
-                        textVost.Text = "ЗМЕЯ";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 6)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/horse.png"));
-                        textVost.Text = "ЛОШАДЬ";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 7)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/goat.png"));
-                        textVost.Text = "КОЗА";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 8)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/monkey.png"));
-                        textVost.Text = "ОБЕЗЬЯНА";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 9)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/cock.png"));
-                        textVost.Text = "ПЕТУХ";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 10)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/dog.png"));
-                        textVost.Text = "СОБАКА";
-                        textVost.Foreground = Brushes.Red;
-
-                    }
-                    if (sign == 11)
-                    {
-                        signsVost.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/Resources/hog.png"));
-                        textVost.Text = "КАБАН";
-                        textVost.Foreground = Brushes.Red;
-
+                        case 0:
+                            u.signEast = "Крыса";
+                            break;
+                        case 1:
+                            u.signEast = "Бык";
+                            break;
+                        case 2:
+                            u.signEast = "Тигр";
+                            break;
+                        case 3:
+                            u.signEast = "Кролик";
+                            break;
+                        case 4:
+                            u.signEast = "Дракон";
+                            break;
+                        case 5:
+                            u.signEast = "Змея";
+                            break;
+                        case 6:
+                            u.signEast = "Лошадь";
+                            break;
+                        case 7:
+                            u.signEast = "Коза";
+                            break;
+                        case 8:
+                            u.signEast = "Обезьяна";
+                            break;
+                        case 9:
+                            u.signEast = "Петух";
+                            break;
+                        case 10:
+                            u.signEast = "Собака";
+                            break;
+                        case 11:
+                            u.signEast = "Кабан";
+                            break;
                     }
                 }
-            }
-                string surnameArr = u.surname;
-                string nameArr = u.name;
-                string midnameArr = u.midname;
-                string telephoneNumberArr = u.telephoneNumber;
-                u.password = $"{surnameArr[0]}{nameArr[(nameArr.Length - 1) / 2]}{midnameArr[midnameArr.Length - 1]}{telephoneNumberArr[telephoneNumberArr.Length - 1]}{telephoneNumberArr[telephoneNumberArr.Length - 2]}{telephoneNumberArr[telephoneNumberArr.Length - 3]}";
-                Users newUsers = new Users() { surname = surnameArr, name = nameArr, midname = midnameArr, telephoneNumber = telephoneNumberArr, password = u.password };
-                L[i] = newUsers;
+                L[i] = u;
             }
         }
 
         static void Main(string[] args)
         {
             List<Users> users = new List<Users>();
-
             getData("horoscopeEastern.csv", users);
+            checkSigns(users);
             printData(users);
-            createNewPassword(users);
             Console.WriteLine();
-            printData(users);
-            createNewFile("users1.csv", users);
+            createNewFile("usersSigns.csv", users);
 
         }
+
+
+
     }
+
 }
+
+
+
